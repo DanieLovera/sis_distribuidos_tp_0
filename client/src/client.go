7a56bb5bf8ed563f34d9752d0403f31108123bd5
Daniel Lovera <daanilo2497@gmail.com>
2024-03-25 17:16:50 -0300

    refactor: use of protocol to send bet message to the server

--- client/src/client.go
+++ client/src/client.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"os/signal"
+	"strconv"
 	"syscall"
 	"time"
 
 	"github.com/7574-sistemas-distribuidos/docker-compose-init/client/src/common"
-	"github.com/7574-sistemas-distribuidos/docker-compose-init/client/src/comms/betmsg"
+	"github.com/7574-sistemas-distribuidos/docker-compose-init/client/src/comms"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -40,22 +40,13 @@ func NewClient(config ClientConfig) *Client {
 // failure, error is printed in stdout/stderr and exit 1
 // is returned
 func (c *Client) createSocket() error {
-	socket := common.NewSocketTcp()
-	err := socket.Connect(c.config.ServerAddress)
-	if err != nil {
-		log.Fatalf(
-			"action: connect | result: fail | client_id: %v | error: %v",
-			c.config.ID,
-			err,
-		)
-	}
-	c.socket = socket
-	return nil
+	c.socket = common.NewSocketTcp()
+	err := c.socket.Connect(c.config.ServerAddress)
+	return err
 }
 
 // StartClientLoop Send messages to the client until some time threshold is met
 func (c *Client) StartClientLoop() {
-	// autoincremental msgID to identify every message sent
 	signalChannel := make(chan os.Signal, 1)
 	signal.Notify(signalChannel, syscall.SIGTERM)
 	betReader := common.GetBetReaderInstance()
@@ -65,23 +56,20 @@ loop:
 	for timeout := time.After(c.config.LoopLapse); ; {
 		bet, err := betReader.Read()
 		if err != nil {
-			// log.Errorf("action: read_bet | result: fail | client_id: %v | bet_sequence: %v", c.config.ID, bet.Sequence)
 			log.Errorf("action: read_bet | result: fail | client_id: %v", c.config.ID)
 			continue
 		}
 
 		err = c.createSocket()
 		if err != nil {
-			log.Fatalf(
-				"action: connect | result: fail | client_id: %v | error: %v",
-				c.config.ID,
-				err,
-			)
+			log.Fatalf("action: connect | result: fail | client_id: %v | error: %v", c.config.ID, err)
+			continue
 		}
+
 		defer c.socket.Close()
-		// Process the client in a goroutine to avoid blocking operations
 		join := make(chan uint32, 1)
 		go c.processClient(join, bet)
+
 		// Wait until timeout, signal or join from the processClient
 		select {
 		case <-timeout:
@@ -100,26 +88,11 @@ loop:
 }
 
 func (c *Client) processClient(join chan uint32, bet common.BetDto) {
-	bms := betmsg.NewSendBetMsg(&bet)
-	se, _ := bms.Serialize()
+	protocol := comms.NewProtocol(&c.socket)
+	id, _ := strconv.ParseUint(c.config.ID, 10, 16)
+	bet.BettingHouseId = uint16(id)
+	protocol.SendBet(bet)
 
-	stream := []byte(fmt.Sprintf("[CLIENT %v] Message N°%v\n", c.config.ID, se))
-	c.socket.Send(stream)
-	buff := make([]byte, len(stream))
-	err := c.socket.Recv(buff)
-
-	if err != nil {
-		log.Errorf("action: receive_message | result: fail | client_id: %v | error: %v",
-			c.config.ID,
-			err,
-		)
-		return
-	}
-	log.Infof("action: receive_message | result: success | client_id: %v | msg: %v",
-		c.config.ID,
-		string(buff),
-	)
-	// Wait a time between sending one message and the next one
 	time.Sleep(c.config.LoopPeriod)
 	join <- bet.Document
 }
