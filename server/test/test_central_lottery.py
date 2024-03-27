3a6f4d71a1583e3342f7b31b4b177eaaecc2a621
Daniel Lovera <daanilo2497@gmail.com>
2024-03-26 21:05:38 -0300

    fix: change test method name from test_central_lottery that were equally named

--- server/test/test_central_lottery.py
+++ server/test/test_central_lottery.py
@@ -3,32 +3,30 @@ import unittest
 from src.common.central_lottery import *
 
 class TestUtils(unittest.TestCase):
+    def setUp(self):
+        self.b1 = Bet('0', 'first_0', 'last_0', '10000000','2000-12-20', LOTTERY_WINNER_NUMBER)
+        self.b2 = Bet('1', 'first_1', 'last_1', '10000001','2000-12-21', LOTTERY_WINNER_NUMBER + 1)
+
     def tearDown(self):
         if os.path.exists(STORAGE_FILEPATH):
             os.remove(STORAGE_FILEPATH)
 
     def test_has_won_with_winner_number_must_be_true(self):
-        b = bet('1', 'first', 'last', 10000000,'2000-12-20', LOTTERY_WINNER_NUMBER)
-        self.assertTrue(has_won(b))
+        self.assertTrue(has_won(self.b1))
 
-    def test_has_won_with_winner_number_must_be_true(self):
-        b = Bet('1', 'first', 'last', 10000000,'2000-12-20', LOTTERY_WINNER_NUMBER + 1)
-        self.assertFalse(has_won(b))
+    def test_has_won_with_loser_number_must_be_false(self):
+        self.assertFalse(has_won(self.b2))
 
     def test_store_bets_and_load_bets_keeps_fields_data(self):
-        to_store = [Bet('1', 'first', 'last', '10000000','2000-12-20', 7500)]
+        to_store = [self.b1]
         store_bets(to_store)
         from_load = list(load_bets())
 
         self.assertEqual(1, len(from_load))
         self._assert_equal_bets(to_store[0], from_load[0])
 
-
     def test_store_bets_and_load_bets_keeps_registry_order(self):
-        to_store = [
-            Bet('0', 'first_0', 'last_0', '10000000','2000-12-20', 7500),
-            Bet('1', 'first_1', 'last_1', '10000001','2000-12-21', 7501),
-        ]
+        to_store = [self.b1, self.b2]
         store_bets(to_store)
         from_load = list(load_bets())
 
